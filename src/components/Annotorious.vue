1dda5c1cfe5c5d6d022be535d980b8ef35721270
fatelei <fatelei@gmail.com>
2018-11-13 18:15:14 +0800

    Fix callback image is blob.

--- src/components/Annotorious.vue
+++ src/components/Annotorious.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h3 v-if="err !== null">{{err}}</h3>
-    <img v-if="imageData" :src="imageData" alt="图片" :ref="key"/>
+    <loading
+      :active.sync="isLoading"
+      :is-full-page="isFullPage"/>
+    <h3 v-if="err">{{ err }}</h3>
+    <img v-if="imageData" :src="imageData" alt="图片" :key="image" ref="image"/>
   </div>
 </template>
 
 
 <script>
 import annotoriouseLoader from './AnnotoriousLoader'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 import 'whatwg-fetch'
 
 export default {
   name: 'Annotorious',
+
+  components: {
+    Loading
+  },
+
   props: {
     image: String,
     onAnnotationCreate: Function,
@@ -25,36 +35,40 @@ export default {
       imageData: null,
       err: null,
       anno: null,
-      key: 0
+      key: 0,
+      isLoading: false,
+      isFullPage: false
     }
   },
 
   mounted: function () {
-    this.loadImage()
+    this.loadImage();
   },
 
   watch: {
     image: function (newValue, oldValue) {
       if (newValue !== oldValue) {
-        this.key += 1
-        this.loadImage()
+        this.loadImage();
       }
     }
   },
 
   methods: {
     loadImage: function () {
+      this.isLoading = true;
       fetch(this.image).then(res => res.blob()).then(imageBinary => {
         this.imageData = URL.createObjectURL(imageBinary)
-        this.fetchAnnotorious()
-        this.err = null
+        this.fetchAnnotorious();
+        this.err = null;
+        this.isLoading = false;
       }).catch(err => {
-        this.err = err
-      })
+        this.err = err;
+        this.isLoading = false;
+      });
     },
 
     fetchAnnotorious: function () {
-      annotoriouseLoader.load(this.srcPath, this.afterLoadAnnotorious)
+      annotoriouseLoader.load(this.srcPath, this.afterLoadAnnotorious);
     },
 
     afterLoadAnnotorious: function () {
@@ -62,37 +76,38 @@ export default {
         this.anno = window.anno;
       }
 
-      this.anno.makeAnnotatable(this.$refs[this.key]);
+      this.anno.makeAnnotatable(this.$refs.image);
 
       this.onCreate.bind(this);
       this.onRemove.bind(this);
       this.onUpdate.bind(this);
       this.anno.addHandler('onAnnotationCreated', this.onCreate);
-      this.anno.addHandler('onAnnotationRemoved', this.onCreate);
-      this.anno.addHandler('onAnnotationUpdated', this.onCreate);
+      this.anno.addHandler('onAnnotationRemoved', this.onRemove);
+      this.anno.addHandler('onAnnotationUpdated', this.onUpdate);
     },
 
     onCreate: function (annotation) {
-      const { src, shapes, text } = annotation
+      const { shapes, text } = annotation;
 
       if (this.onAnnotationCreate) {
-        this.onAnnotationCreate(src, shapes[0].geometry, text)
+        this.onAnnotationCreate(this.image, shapes[0].geometry, text);
       }
     },
 
     onRemove: function (annotation) {
-      const { src, shapes, text } = annotation
+      const { shapes, text } = annotation;
+      
 
       if (this.onAnnotationRemove) {
-        this.onAnnotationRemove(src, shapes[0].geometry, text)
+        this.onAnnotationRemove(this.image, shapes[0].geometry, text);
       }
     },
 
     onUpdate: function (annotation) {
-      const { src, shapes, text } = annotation
+      const { shapes, text } = annotation;
 
       if (this.onAnnotationUpdate) {
-        this.onAnnotationUpdate(src, shapes[0].geometry, text)
+        this.onAnnotationUpdate(this.image, shapes[0].geometry, text);
       }
     }
   }
@@ -101,9 +116,5 @@ export default {
 
 <style scoped>
 @import '../../node_modules/annotorious/css/theme-dark/annotorious-dark.css';
-
-img {
-  width: 100%;
-}
 </style>
 
